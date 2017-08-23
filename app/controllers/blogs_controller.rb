e33fc18ff42f35cc438873e13271560fdf07733c
wen <sunzibinfa@gmail.com>
2017-08-23 23:11:53 +0800

    action of edit and update

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,6 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:edit, :update, :show, :destroy]
+
   def index
     @blog = Blog.all
   end
@@ -17,8 +19,23 @@ class BlogsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @blog.update(params_blog)
+      redirect_to :blogs
+    else
+      render :new
+    end
+  end
+
   private
     def params_blog
       params.require(:blog).permit(:title, :content)
+    end
+
+    def set_blog
+      @blog = Blog.find(params[:id])
     end
 end
