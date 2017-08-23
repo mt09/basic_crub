5dc6ab65f51fdafa17245e7277a0895be935a594
wen <sunzibinfa@gmail.com>
2017-08-23 22:59:52 +0800

    new and create actions

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,24 @@
 class BlogsController < ApplicationController
   def index
+    @blog = Blog.all
   end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(params_blog)
+
+    if @blog.save
+      redirect_to :blogs
+    else
+      render :new
+    end
+  end
+
+  private
+    def params_blog
+      params.require(:blog).permit(:title, :content)
+    end
 end
